0f5d2eda6b29e48c221d78316808ecb6ba6ea564
Vira Murray <vira.avramenko@gmail.com>
2022-09-12 13:14:44 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//syntax
+string dogName = "Lucy"; //declaring a variable of type string
+string dogBreed = "chihuahua";
+
+int age = 1;
+
+char someChar = 'a';
+bool isEsa = true;
+double weight = 6.5;
+decimal vetBill = 100m;
+Console.WriteLine($"My dog's name is  {dogName}, she is {someChar} {age} year old {weight} pound {dogBreed} with a vet bill of {vetBill} USD.");
+
+
+
+
+
